bc5e23f50101fa257b3667ef6ba1f112d10726cd
Danilo Desole <danilodesole@gmail.com>
2021-10-03 16:53:47 +0100

    Adding route

--- backend/api/restaurant/controllers/restaurant.js
+++ backend/api/restaurant/controllers/restaurant.js
@@ -1,8 +1,24 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  // Defining a new handler
+  // The ctx input object contains the route parameters
+  findByCategory: async (ctx) => {
+    const { categoryId } = ctx.params;
+    const restaurantsByCategory = await strapi
+      .query("restaurant")
+      .find({ categories: [categoryId] });
+
+    return restaurantsByCategory.map((restaurant) =>
+      sanitizeEntity(restaurant, {
+        model: strapi.models.restaurant,
+      })
+    );
+  },
+};
